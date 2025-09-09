e09ec669fe73f93b0028b4baecdf190b99f71631
Perez Endale <your@email.com>
2025-09-09 13:41:14 +0300

    1

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000  # Principal amount in dollars
+rate = 0.05       # Interest rate per year (5%)
+time = 3          # Time in years
+
+interest = principal * rate * time
+
+print(f'Simple interest for principal ${principal} at rate {rate*100}% for {time} years is ${interest}')
