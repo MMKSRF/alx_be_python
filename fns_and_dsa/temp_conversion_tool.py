c7affa0fbf509429b06bb748595fd6ced461070d
MMKSRF <perezendale247@gmail.com>
2025-09-22 13:14:05 +0300

    8

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,15 +1,13 @@
 # temp_conversion_tool.py
 
 # Global conversion factors
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
 def convert_to_celsius(fahrenheit):
-    # Use global conversion factor (reading only, no need for 'global' keyword)
     return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
-    # Use global conversion factor (reading only, no need for 'global' keyword)
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
@@ -30,3 +28,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
